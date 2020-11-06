cfd5bbdf401277d97ff9d6262a747931abfda690
RRapids <tengfeiqaq@gmail.com>
2020-11-06 22:42:33 +0800

    新增管理员

--- app/extend/context.js
+++ app/extend/context.js
@@ -1,5 +1,4 @@
 'use strict';
-const crypto = require('crypto');
 module.exports = {
   // 成功提示
   apiSuccess(data = '', msg = 'ok', code = 200) {
@@ -11,32 +10,100 @@ module.exports = {
     this.body = { msg, data };
     this.status = code;
   },
-  // 生成token
-  getToken(value) {
-    return this.app.jwt.sign(value, this.app.config.jwt.secret);
-  },
-  // 验证token
-  checkToken(token) {
-    return this.app.jwt.verify(token, this.app.config.jwt.secret);
-  },
-  // 验证密码
-  async checkPassword(password, hash_password) {
-    // 先对需要验证的密码进行加密
-    const hmac = crypto.createHash('sha256', this.app.config.crypto.secret);
-    hmac.update(password);
-    password = hmac.digest('hex');
-    const res = password === hash_password;
-    if (!res) {
-      this.throw(400, '密码错误');
+  // 分页
+  async page(modelName, where = {}, options = {}) {
+    const page = this.query.page ? parseInt(this.query.page) : 1;
+    const limit = this.query.limit ? parseInt(this.query.limit) : 10;
+    const offset = (page - 1) * limit;
+
+
+    if (!options.order) {
+      options.order = [
+        [ 'id', 'DESC' ],
+      ];
     }
-    return true;
+
+
+    const res = await this.app.model[modelName].findAndCountAll({
+      where,
+      offset,
+      limit,
+      ...options,
+    });
+
+
+    // 总页数
+    const totalPage = Math.ceil(res.count / limit);
+
+
+    let query = { ...this.query };
+    if (query.hasOwnProperty('page')) {
+      delete query.page;
+    }
+    if (query.hasOwnProperty('limit')) {
+      delete query.limit;
+    }
+
+
+    // 对象转&拼接字符串
+    const urlEncode = (param, key, encode) => {
+      if (param == null) return '';
+      let paramStr = '';
+      const t = typeof (param);
+      if (t == 'string' || t == 'number' || t == 'boolean') {
+        paramStr += '&' + key + '=' + ((encode == null || encode) ? encodeURIComponent(param) : param);
+      } else {
+        for (const i in param) {
+          const k = key == null ? i : key + (param instanceof Array ? '[' + i + ']' : '.' + i);
+          paramStr += urlEncode(param[i], k, encode);
+        }
+      }
+      return paramStr;
+    };
+
+
+    query = urlEncode(query);
+
+
+    let pageEl = '';
+    for (let index = 1; index <= totalPage; index++) {
+      let active = '';
+      if (page === index) {
+        active = 'active';
+      }
+      pageEl += `
+            <li class="page-item ${active}">
+            <a class="page-link" href="?page=${index}&limit=${limit}${query}">${index}</a></li>
+            `;
+    }
+
+    const preDisabled = page <= 1 ? 'disabled' : '';
+    const nextDisabled = page >= totalPage ? 'disabled' : '';
+
+
+    const pageRender = `
+        <ul class="pagination">
+            <li class="page-item ${preDisabled}">
+                <a class="page-link" href="?page=${page - 1}&limit=${limit}${query}" aria-label="Previous">
+                    <span aria-hidden="true">«</span>
+                    <span class="sr-only">Previous</span>
+                </a>
+            </li>
+            ${pageEl}
+            <li class="page-item ${nextDisabled}">
+                <a class="page-link" href="?page=${page + 1}&limit=${limit}${query}" aria-label="Next">
+                    <span aria-hidden="true">»</span>
+                <span class="sr-only">Next</span>
+                </a>
+            </li>
+        </ul>
+        `;
+
+
+    this.locals.pageRender = pageRender;
+
+
+    return res.rows;
   },
-  // 生成随机字符串
-  randomString(length) {
-    const chars =
-      '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let result = '';
-    for (let i = length; i > 0; --i) { result += chars[Math.floor(Math.random() * chars.length)]; }
-    return result;
-  },
+
 };
